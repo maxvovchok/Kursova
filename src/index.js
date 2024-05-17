d7a7bef240a0996113c1499f32b2f0c9593136fe
maxvovchok <vovchokmakcim@gmail.com>
2024-05-17 16:19:50 +0300

    test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/kursova">
       <Provider store={store}>
         <App />
       </Provider>
